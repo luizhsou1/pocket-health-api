6dd31c1da95aed0b00312acb4f7ee965dca53596
Luiz Henrique <luizhsouza97@outlook.com>
2019-11-26 08:29:46 -0300

    Vou Começar a fazer mudanças drasticas no model, para adaptar aos métodos novos do mongoose

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,38 +1,55 @@
 'use strict';
-function UserDAO(model) {
-  this.model = model;
+
+// TODO Me baseando no artigo https://scotch.io/@vigneshsithirai/basic-crud-operation-with-nodejs-express-and-mongodb
+// Só que no meu UserModel, devolvo um DAO, que a união das camadas service e model dele 
+
+class  UserDAO {
+  constructor(UserModel) {
+    this.model = UserModel; 
+  }
+
+  create(data, callback) {
+    var model = new this.model(data);
+    model.save(function(err, result) {
+      callback(err, result);
+    });
+  }
+  
+  find(query, callback) {
+    this.model.find(query).exec(callback);
+  }
+  
+  findOne(_id, callback) {
+    var query = { _id : _id };
+    this.model.findOne(query).exec(callback);
+  }
+  
+  update(_id, data, callback) {
+    var query = { _id : _id };
+    this.model.update(query, data).exec(function(err, result) {
+      callback(err, result);
+    });
+  }
+  
+  remove(_id, callback) {
+    var query = { _id : _id };
+    this.model.remove(query).exec(function(err, result) {
+      callback(err, result);
+    });
+  }
 }
-UserDAO.prototype.create = function(data, callback) {
-  var model = new this.model(data);
-  model.save(function(err, result) {
-    callback(err, result);
-  });
-};
-UserDAO.prototype.find = function(query, callback) {
-  this.model.find(query).exec(callback);
-};
-UserDAO.prototype.findOne = function(_id, callback) {
-  var query = { _id : _id };
-  this.model.findOne(query).exec(callback);
-};
-UserDAO.prototype.update = function(_id, data, callback) {
-  var query = { _id : _id };
-  this.model.update(query, data).exec(function(err, result) {
-    callback(err, result);
-  });
-};
-UserDAO.prototype.remove = function(_id, callback) {
-  var query = { _id : _id };
-  this.model.remove(query).exec(function(err, result) {
-    callback(err, result);
-  });
-};
+
+
+
 module.exports = function(mongoose) {
-  var User = mongoose.model('User', {
+  const UserSchema = new mongoose.Schema({
     userName: { type: String, required: true },
     password: { type: String, required: true },
     name: { type: String, required: true },
     type: String 
   });
-  return new UserDAO(User);
+
+  const UserModel = mongoose.model('User', UserSchema);
+
+  return new UserDAO(UserModel);
 };
